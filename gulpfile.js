1d040191a97cc79f849d9dfa316761289496025f
imre-oander <imre.toth@oander.hu>
2017-04-07 13:23:33 +0200

    Generate new CSS - update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   notify = require('gulp-notify'),
   sourcemaps = require('gulp-sourcemaps'),
-  autoprefixer  = require('gulp-autoprefixer'),
-  browserSync = require('browser-sync');
+  autoprefixer  = require('gulp-autoprefixer');
 
 var dist = 'app/design/frontend/Oander/istyle/web/';
 var distCss = dist + 'css/';
@@ -20,8 +19,24 @@ gulp.task('sass', function() {
   return gulp.src(paths.scss)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 versions'))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    })).on('error', notify.onError(function (error) {
+      return error.message;
+    }))
+    .pipe(autoprefixer({
+      browsers: [
+        'Chrome >= 45',
+        'Firefox >= 45',
+        'Edge >= 12',
+        'Explorer >= 9',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12'
+      ]
+    }))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(distCss));
 });
