7115c1eaaf97cdf15e44f9b37096572d67f7e7e1
gaborkuti88 <gabor.kuti@oander.hu>
2017-04-18 12:10:48 +0200

    add algoliasearch module

--- app/etc/config.php
+++ app/etc/config.php
@@ -2,6 +2,7 @@
 return array (
   'modules' => 
   array (
+    'Algolia_AlgoliaSearch' => 1,
     'Magento_Store' => 1,
     'Magento_AdvancedPricingImportExport' => 1,
     'Magento_Directory' => 1,
