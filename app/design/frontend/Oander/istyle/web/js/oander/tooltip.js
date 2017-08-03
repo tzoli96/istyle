52ba29e51fa30bc8563e0b4a04ded595e4dace72
Tamas Laszlo <tamas.laszlo@oander.hu>
2017-08-03 11:02:53 +0200

    recently viewed products panel

---
+++ app/design/frontend/Oander/istyle/web/js/oander/tooltip.js
@@ -0,0 +1,19 @@
+/**
+ * @author Tamas Laszlo
+ * @copyright OANDER Media Kft.
+ * @project istyle
+ * @date  2017.08.03
+ */
+
+define([
+    'jquery',
+    'mage/bootstrap'
+], function ($) {
+    'use strict';
+
+    // Enable Bootstrap-tooltip for the whole webpage
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+});
