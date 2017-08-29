b85528287312d28c1ff31695bab06fce9a9f5a01
Turupoli István <turupoli.istvan@gmail.com>
2017-08-29 18:41:10 +0200

    small fix

--- app/design/frontend/Oander/istyle/requirejs-config.js
+++ app/design/frontend/Oander/istyle/requirejs-config.js
@@ -6,14 +6,14 @@ var config = {
     '*': {
       'oander.navigation': 'js/oander/navigation',
       'oander.accordion': 'js/oander/accordion',
-      'oander.label-replace': 'js/oander/label-replace',
       'oander.items-slider': 'js/oander/items-slider',
       'oander.slider-configuration': 'js/oander/slider-configuration',
       'oander.tooltip': 'js/oander/tooltip',
       'oander.custom-number-input': 'js/oander/custom-number-input',
+      'oander.label-replace': 'js/oander/label-replace',
       relatedProducts: 'js/module-catalog/related-products',
       upsellProducts: 'js/module-catalog/upsell-products',
-      oanderWarehouseView: 'js/oander/warehouse-manager/product/view',
+      oanderWarehouseView: 'js/oander/warehouse-manager/product/view'
     }
   }
 };
