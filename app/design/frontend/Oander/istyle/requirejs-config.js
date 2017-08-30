d53cb1661391fc2d7a76da006e4f61b6d76180de
Tamas Laszlo <tamas.laszlo@oander.hu>
2017-08-30 14:10:09 +0200

    iMagazin widget formatted

--- app/design/frontend/Oander/istyle/requirejs-config.js
+++ app/design/frontend/Oander/istyle/requirejs-config.js
@@ -10,6 +10,7 @@ var config = {
       'oander.slider-configuration': 'js/oander/slider-configuration',
       'oander.tooltip': 'js/oander/tooltip',
       'oander.custom-number-input': 'js/oander/custom-number-input',
+      'oander.article-offer': 'js/oander/article-offer',
       relatedProducts: 'js/module-catalog/related-products',
       upsellProducts: 'js/module-catalog/upsell-products',
       oanderWarehouseView: 'js/oander/warehouse-manager/product/view',
