17553713b6d6bc62de8994b4af49fd1d5aca42d2
Attila Kasza <kasza.attila86@gmail.com>
2017-12-06 16:47:20 +0100

    fix mount points to EFS

--- bin/install.sh
+++ bin/install.sh
@@ -3,12 +3,12 @@ WEBROOT="/var/www/istyle.mk/webroot"
 
 #php bin/magento maintenance:enable 
 
-cp /mnt/efs/istyle.mk/env/env.php ${WEBROOT}/app/etc
-ln -s /mnt/efs/istyle.mk/var/media ${WEBROOT}/media
+cp /mnt/efs/istyle/env/env.php ${WEBROOT}/app/etc
+ln -s /mnt/efs/istylek/var/media ${WEBROOT}/media
 if [ -d ${WEBROOT}/pub/static];then rmdir ${WEBROOT}/pub/static;else rm ${WEBROOT}/pub/static;fi
-ln -s /mnt/efs/istyle.mk/pub/static ${WEBROOT}/pub/static
+ln -s /mnt/efs/istyle/pub/static ${WEBROOT}/pub/static
 if [ -d ${WEBROOT}/var ];then rmdir ${WEBROOT}/var;else rm ${WEBROOT}/var;fi
-ln -s /mnt/efs/istyle.mk/var ${WEBROOT}/var
+ln -s /mnt/efs/istyle/var ${WEBROOT}/var
 
 INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 MASTER_ID="i-0a57263aca752890a"
