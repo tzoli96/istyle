d6dd98e411ae156d49db6c25991e8ae9642dd054
Attila Kasza <kasza.attila86@gmail.com>
2017-12-07 16:28:59 +0100

    fix install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,10 +3,10 @@ WEBROOT="/var/www/istyle.eu/webroot"
 
 #/usr/bin/php /var/www/istyle.eu/webroot/bin/magento maintenance:enable 
 
-cp /mnt/efs/istyle/env/env.php ${WEBROOT}/app/etc
+cp /mnt/efs/istyle/env/env.php ${WEBROOT}/app/etc/
 ln -s /mnt/efs/istyle/media/ ${WEBROOT}/media
 if [ -d ${WEBROOT}/pub/static];then rmdir ${WEBROOT}/pub/static;else rm ${WEBROOT}/pub/static;fi
-ln -s /mnt/efs/istyle/pub/static ${WEBROOT}/pub/static
+ln -s /mnt/efs/istyle/pub/static/ ${WEBROOT}/pub/
 if [ -d ${WEBROOT}/var ];then rmdir ${WEBROOT}/var;else rm ${WEBROOT}/var;fi
 ln -s /mnt/efs/istyle/var ${WEBROOT}/var
 
@@ -25,6 +25,7 @@ if [ "${INSTANCE_ID}" == "${MASTER_ID}" ]
 	else 
 		#WORKER BRANCH
 		/usr/bin/php /var/www/istyle.eu/webroot/bin/magento maintenance:enable
+		sleep 10
 		/bin/su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && /usr/bin/composer install"
 		/usr/bin/php /var/www/istyle.eu/webroot/bin/magento maintenance:disable
 fi
