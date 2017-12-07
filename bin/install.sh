564448de928426497d982a0e576f05dbdf647041
Attila Kasza <kasza.attila86@gmail.com>
2017-12-07 12:40:23 +0100

    reworking install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WEBROOT="/var/www/istyle.mk/webroot"
+WEBROOT="/var/www/istyle.eu/webroot"
 
 #php bin/magento maintenance:enable 
 
@@ -16,16 +16,16 @@ MASTER_ID="i-0a57263aca752890a"
 if [ "${INSTANCE_ID}" == "${MASTER_ID}" ]
 	then 
 		#MASTER WORKFLOW
-		#su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&npm install"
-		su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&composer install"
-		su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&php bin/magento setup:upgrade"
-		#su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&php bin/magento setup:di:compile"
-		su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&php bin/magento setup:static-content:deploy"
-		su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&php bin/magento setup:upgrade --keep-generated "
+#		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && npm install"
+		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && composer install"
+		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && /usr/bin/php bin/magento setup:upgrade"
+#		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && /usr/bin/php bin/magento setup:di:compile"
+		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && /usr/bin/php bin/magento setup:static-content:deploy"
+		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && /usr/bin/php bin/magento setup:upgrade --keep-generated "
 
 	else 
 		#WORKER BRANCH
-		su - www-data -s /bin/bash -c "cd /var/www/istyle.mk/webroot&&composer install"
+		su - www-data -s /bin/bash -c "cd /var/www/${WEBROOT} && composer install"
 fi
 
 chown www-data:www-data -R ${WEBROOT}
