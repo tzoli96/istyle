021efbbf70a25f599730f523c86cafbdb07ddebb
Attila Kasza <kasza.attila86@gmail.com>
2017-12-11 12:58:39 +0100

    install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -18,16 +18,20 @@ if [ "${INSTANCE_ID}" == "${MASTER_ID}" ]
 		#MASTER WORKFLOW
 #		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/npm install"
 		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/composer install"
+		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento maintenance:enable
 		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:upgrade"
-#		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:di:compile"
-		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:static-content:deploy"
-		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:upgrade --keep-generated "
+		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:di:compile"
+		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:static-content:deploy en_US"
+		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:static-content:deploy mk_MK"
+#		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:static-content:deploy"
+#		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento setup:upgrade --keep-generated"
+		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/php bin/magento maintenance:disable
 	else 
 		#WORKER BRANCH
-		/usr/bin/php ${WEBROOT}/bin/magento maintenance:enable
-		sleep 10
+#		/usr/bin/php ${WEBROOT}/bin/magento maintenance:enable
+#		sleep 10
 		/bin/su - www-data -s /bin/bash -c "cd ${WEBROOT} && /usr/bin/composer install"
-		/usr/bin/php ${WEBROOT}/bin/magento maintenance:disable
+#		/usr/bin/php ${WEBROOT}/bin/magento maintenance:disable
 fi
 
 /bin/chown www-data:www-data -R ${WEBROOT}
