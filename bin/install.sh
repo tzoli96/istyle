1ff3907edf6cd79e1240382f10fa3a5c55df6091
Attila Kasza <kasza.attila86@gmail.com>
2017-12-15 11:54:57 +0100

    install.sh v0.1

--- bin/install.sh
+++ bin/install.sh
@@ -2,42 +2,75 @@
 
 WEBROOT="/var/www/istyle.eu/webroot"
 EFS="/mnt/efs/istyle"
-
-cp ${EFS}/env/env.php ${WEBROOT}/app/etc/
+EFS_BLUE="/mnt/efs/istyle/blue"
+EFS_GREEN="/mnt/efs/istyle/green"
+INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+MASTER_ID="i-0a57263aca752890a"
 
 [ -L ${WEBROOT}/pub/media ] && rm ${WEBROOT}/pub/media
 ln -s ${EFS}/media ${WEBROOT}/pub/
 
-[ -L ${WEBROOT}/pub/static ] && rm ${WEBROOT}/pub/static || rm -rf ${WEBROOT}/pub/static
-ln -s ${EFS}/pub/static ${WEBROOT}/pub/
+if [ "${INSTANCE_ID}" == "${MASTER_ID}" ]; then
+   # MASTER WORKFLOW #
+   # RSYNC BLUE FOLDER TO GREEN WITH EXCEPTIONS
+   rsync -aur --exclude={"/var/backups/*","/var/generation/*","/var/di/*","/pub/static/*"} ${EFS_BLUE}/* ${EFS_GREEN}/
 
-[ -L ${WEBROOT}/var ] && rm ${WEBROOT}/var
-ln -s ${EFS}/var ${WEBROOT}/
+   # CREATE SYMLINKS TO GREEN
+   [ -L ${WEBROOT}/var ] && rm ${WEBROOT}/var
+   ln -s ${EFS_GREEN}/var ${WEBROOT}/
+   [ -L ${WEBROOT}/pub/static ] && rm ${WEBROOT}/pub/static || rm -rf ${WEBROOT}/pub/static
+   ln -s ${EFS_GREEN}/pub/static ${WEBROOT}/pub/
 
-INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-MASTER_ID="i-0a57263aca752890a"
+   # INSTALL CODE
+   # cd ${WEBROOT} && npm install
+   cd ${WEBROOT} && composer install
+   cd ${WEBROOT} && php bin/magento maintenance:enable
 
-if [ "${INSTANCE_ID}" == "${MASTER_ID}" ]
-	then 
-		#MASTER WORKFLOW
-#		cd ${WEBROOT} && /usr/bin/npm install
-		cd ${WEBROOT} && /usr/bin/composer install
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento maintenance:enable
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento setup:upgrade
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento setup:di:compile
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento setup:static-content:deploy en_US
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento setup:static-content:deploy mk_MK
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento setup:static-content:deploy
-#		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento setup:upgrade --keep-generated
-		cd ${WEBROOT} && /usr/bin/php ${WEBROOT}/bin/magento maintenance:disable
-	else 
-		#WORKER BRANCH
-#		/usr/bin/php ${WEBROOT}/bin/magento maintenance:enable
-#		sleep 10
-		cd ${WEBROOT} && /usr/bin/composer install
-#		/usr/bin/php ${WEBROOT}/bin/magento maintenance:disable
+   # CHECK IF DB UPGRADE NEEDED
+   if php ${WEBROOT}bin/magento setup:db:status | grep -q "up to date"; then
+      # MAGENTO UPGRADE PROCESS WITHOUT DB CHANGE
+      cd ${WEBROOT} && php bin/magento setup:upgrade
+   else
+      # COPY DB
+      mysql -e 'DROP DATABASE istylem2_upg; CREATE DATABASE istylem2_upg;'
+      mysqldump istylem2 | mysql istylem2_upg
+      mysql -e 'DROP DATABASE istylem2p_upg; CREATE DATABASE istylem2p_upg;'
+      mysqldump istylem2p | mysql istylem2p_upg
+
+      # UPDATE THE ENVIRONMENT FILE WITH NEW DATABASES
+      cp ${EFS}/env/upgrade_env.php ${WEBROOT}/app/etc/env.php
+
+      # UPGRADE MAGENTO WITH DB CHANGE
+      cd ${WEBROOT} && php bin/magento setup:upgrade
+   fi
+
+   # CONTINUE MAGENTO UPGRADE PROCESS
+   cd ${WEBROOT} && php bin/magento setup:di:compile
+   cd ${WEBROOT} && php bin/magento setup:static-content:deploy en_US
+   cd ${WEBROOT} && php bin/magento setup:static-content:deploy mk_MK
+   touch ${EFS}/deployed.flag
+   cd ${WEBROOT} && php bin/magento maintenance:disable
+   chown www-data:www-data -R ${EFS_GREEN}
+else
+   # WORKER INSTANCES #
+   cp ${EFS}/env/env.php ${WEBROOT}/app/etc/
+
+   # CREATE SYMLINKS TO BLUE
+   [ -L ${WEBROOT}/var ] && rm ${WEBROOT}/var
+   ln -s ${EFS_BLUE}/var ${WEBROOT}/
+   [ -L ${WEBROOT}/pub/static ] && rm ${WEBROOT}/pub/static || rm -rf ${WEBROOT}/pub/static
+   ln -s ${EFS_BLUE}/pub/static ${WEBROOT}/pub/
+
+   cd ${WEBROOT} && composer install
+
+   if [ -f ${EFS}/deployed.flag ]; then
+      cd ${WEBROOT} && php bin/magento maintenance:enable
+      rsync -aur --exclude={"/var/backups/*"} ${EFS_GREEN}/* ${EFS_BLUE}/
+      cd ${WEBROOT} && php bin/magento setup:upgrade --keep-generated
+      cd ${WEBROOT} && php bin/magento maintenance:disable
+      rm ${EFS}/deployed.flag
+   fi
 fi
 
-/bin/chown www-data:www-data -R /var/www/istyle.eu/
-/bin/chown www-data:www-data -R /mnt/efs/istyle/var/
+chown www-data:www-data -R /var/www/istyle.eu/
 /etc/init.d/php7.0-fpm restart
