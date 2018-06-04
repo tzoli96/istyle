994901abf2fa53d07faf6e6a58c47ad099cece76
vegvari42 <tamas@vegvari.net>
2018-06-04 16:17:15 +0200

    update

--- app/code/Oander/MirasvitFeed/Export/Resolver/ProductResolver.php
+++ app/code/Oander/MirasvitFeed/Export/Resolver/ProductResolver.php
@@ -29,6 +29,8 @@ use Magento\Framework\App\ResourceConnection;
 use Mirasvit\Feed\Export\Context;
 use Magento\Swatches\Helper\Data as SwatchesHelper;
 use Mirasvit\Feed\Model\Dynamic\Attribute;
+use Oander\WarehouseManager\Enum\ProductStock\StockStatus;
+use Oander\WarehouseManager\Helper\ProductStockDisplay;
 
 class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
 {
@@ -36,17 +38,25 @@ class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
      * @var ProductRelation
      */
     private $productRelation;
+
     /**
      * @var ProductMetadata
      */
     private $productMetadata;
+
     /**
      * @var ProductFactory
      */
     private $productFactory;
 
     /**
+     * @var ProductStockDisplay
+     */
+    private $productStockDisplay;
+
+    /**
      * ProductResolver constructor.
+     * @param ProductStockDisplay $productStockDisplay
      * @param StockRegistryInterface $stockRegistry
      * @param ProductRelation $productRelation
      * @param AttributeCollectionFactory $attributeCollectionFactory
@@ -59,13 +69,28 @@ class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
      * @param Attribute $dynamicAttribute
      * @param array $resolvers
      */
-    public function __construct(StockRegistryInterface $stockRegistry, ProductRelation $productRelation, AttributeCollectionFactory $attributeCollectionFactory, ProductFactory $productFactory, ProductMetadata $productMetadata, ResourceConnection $resource, SwatchesHelper $swatchesHelper, Context $context, ObjectManagerInterface $objectManager, Attribute $dynamicAttribute, array $resolvers = [])
-    {
-        parent::__construct($stockRegistry, $productRelation, $attributeCollectionFactory, $productFactory, $productMetadata, $resource, $swatchesHelper, $context, $objectManager, $dynamicAttribute, $resolvers);
+    public function __construct(
+        ProductStockDisplay $productStockDisplay,
+        StockRegistryInterface $stockRegistry,
+        ProductRelation $productRelation,
+        AttributeCollectionFactory $attributeCollectionFactory,
+        ProductFactory $productFactory,
+        ProductMetadata $productMetadata,
+        ResourceConnection $resource,
+        SwatchesHelper $swatchesHelper,
+        Context $context,
+        ObjectManagerInterface $objectManager,
+        Attribute $dynamicAttribute,
+        array $resolvers = []
+    ) {
+        parent::__construct(
+            $stockRegistry, $productRelation, $attributeCollectionFactory, $productFactory,
+            $productMetadata, $resource, $swatchesHelper, $context, $objectManager, $dynamicAttribute, $resolvers);
         $this->productRelation = $productRelation;
         $this->productMetadata = $productMetadata;
         $this->resource = $resource;
         $this->productFactory = $productFactory;
+        $this->productStockDisplay = $productStockDisplay;
     }
 
     /**
@@ -77,20 +102,17 @@ class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
      */
     public function getRealurl($product)
     {
-        if ($product->getTypeId()==\Magento\Catalog\Model\Product\Type::TYPE_SIMPLE)
-        {
+        if ($product->getTypeId() == \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE) {
             $url = $product->getProductUrl();
             /** @var Product $parentproduct */
             $parentproduct = $this->getParent($product);
-            if($parentproduct)
-            {
-                if($parentproduct->getTypeId()==\Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) {
+            if ($parentproduct) {
+                if ($parentproduct->getTypeId() == \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) {
                     $params = array();
                     /** @var \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configtype */
                     $configtype = $parentproduct->getTypeInstance();
                     $configattributes = $configtype->getConfigurableAttributes($parentproduct);
-                    foreach($configattributes as $attribute)
-                    {
+                    foreach ($configattributes as $attribute) {
                         if (!is_null($attribute->getProductAttribute())) {
                             $code = $attribute->getProductAttribute()->getAttributeCode();
                             $params[$code] = $product->getData($code);
@@ -99,9 +121,7 @@ class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
                     $url = $parentproduct->getProductUrl() . '?' . http_build_query($params);
                 }
             }
-        }
-        else
-        {
+        } else {
             $url = $product->getProductUrl();
         }
 
@@ -127,7 +147,7 @@ class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
         if (count($parentIds)) {
             /** @var  $store */
             $websiteid = $this->getFeed()->getStore()->getWebsiteId();
-            $parentIdsstring = implode(',',$parentIds);
+            $parentIdsstring = implode(',', $parentIds);
             $select = $this->resource->getConnection()->select()->from(
                 $this->resource->getTableName('catalog_product_website'),
                 ['product_id']
@@ -135,16 +155,41 @@ class ProductResolver extends \Mirasvit\Feed\Export\Resolver\ProductResolver
                 "product_id IN ({$parentIdsstring}) and website_id = {$websiteid}"
             );
             $websiteparentIds = $this->productRelation->getConnection()->fetchCol($select);
-            if(count($websiteparentIds))
-            {
+            if (count($websiteparentIds)) {
                 return $this->productFactory->create()->load($websiteparentIds[0]);
-            }
-            else
-            {
+            } else {
                 return $product;
             }
         } else {
             return $product;
         }
+    }
+
+    /**
+     * Return product QTY
+     *
+     * @param Product $product
+     * @return int
+     */
+    public function getQty($product)
+    {
+        $websiteId = $this->getFeed()->getStore()->getWebsiteId();
+        $productStock = $this->productStockDisplay->getProductStock((int)$product->getId(), (int)$websiteId);
+
+        return (int)$productStock->getWebsiteQty();
+    }
+
+    /**
+     * Return product stock status
+     *
+     * @param Product $product
+     * @return int
+     */
+    public function getIsInStock($product)
+    {
+        $websiteId = $this->getFeed()->getStore()->getWebsiteId();
+        $productStock = $this->productStockDisplay->getProductStock((int)$product->getId(), (int)$websiteId);
+
+        return $productStock->getStockStatus() === StockStatus::IN_STOCK;
     }
 }
