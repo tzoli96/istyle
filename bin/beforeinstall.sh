0a6d5068f01d99820ad3b005c946572a86faea02
Attila Kasza <kasza.attila86@gmail.com>
2018-07-16 11:00:53 +0200

    deploy script beforeinstall rework

--- bin/beforeinstall.sh
+++ bin/beforeinstall.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
-set -eu
-
-/etc/init.d/php7.0-fpm stop
-/etc/init.d/nginx stop
 
 WEBROOT="/var/www/istyle.eu/"
 LOGDIR="/var/log/magento"
 
+nfs_check() {
+  if [ ! -f /mnt/istyle-storage/istyle/.nfs.check ]; then
+    echo -n "NFS file not found, trying to remount .. "
+    if mount -a; then echo OK; else echo FAIL; fi
+  fi
+}
+
+cron_check() {
+  echo -n "Checking CRON: "
+  while pgrep -x php7.0 > /dev/null; do
+    echo -n "."
+    sleep 5
+  done
+  echo " DONE!"
+}
+
+cron_check
+
+echo -n "Stop PHP .. "
+if /usr/bin/pkill -9 php; then echo OK; else echo FAIL; fi
+echo -n "Stop Nginx .. "
+/usr/bin/pkill -9 nginx; then echo OK; else echo FAIL; fi
+
+echo -n "Delete and recreate webroot directories .. "
 if [ -d ${WEBROOT} ]; then
     rm -rf ${WEBROOT}
     rm -rf ${LOGDIR}
@@ -17,4 +37,5 @@ mkdir -p ${LOGDIR}/log/oander
 chown www-data:www-data -R /var/www/.composer
 chown www-data:www-data -R ${LOGDIR}
 chown www-data:www-data -R ${WEBROOT}
+echo "OK"
 
