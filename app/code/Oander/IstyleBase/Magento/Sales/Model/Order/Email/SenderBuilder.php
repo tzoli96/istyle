86a688a2a1c4db6ac20d3fe45d4331d242841655
robertbtln <robert.betlen@oander.hu>
2018-08-21 15:16:41 +0200

    #27533 - bugfix - email sender address fix

---
+++ app/code/Oander/IstyleBase/Magento/Sales/Model/Order/Email/SenderBuilder.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ *   /$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$
+ *  /$$__  $$ /$$__  $$| $$$ | $$| $$__  $$| $$_____/| $$__  $$
+ * | $$  \ $$| $$  \ $$| $$$$| $$| $$  \ $$| $$      | $$  \ $$
+ * | $$  | $$| $$$$$$$$| $$ $$ $$| $$  | $$| $$$$$   | $$$$$$$/
+ * | $$  | $$| $$__  $$| $$  $$$$| $$  | $$| $$__/   | $$__  $$
+ * | $$  | $$| $$  | $$| $$\  $$$| $$  | $$| $$      | $$  \ $$
+ * |  $$$$$$/| $$  | $$| $$ \  $$| $$$$$$$/| $$$$$$$$| $$  | $$
+ *  \______/ |__/  |__/|__/  \__/|_______/ |________/|__/  |__/
+ *
+ * Oander_IstyleBase
+ *
+ * SenderBuilder
+ *
+ * @author  Róbert Betlen <robert.betlen@oander.hu>
+ * @license Oander Media Kft. (http://www.oander.hu)
+ */
+
+namespace Oander\IstyleBase\Magento\Sales\Model\Order\Email;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Sales\Model\Order\Email\Container\IdentityInterface;
+use Magento\Sales\Model\Order\Email\Container\Template;
+use Oander\IstyleBase\Magento\Framework\Mail\Template\TransportBuilderByStore;
+
+class SenderBuilder extends \Oander\OrderAttachment\Magento\Sales\Model\Order\Email\SenderBuilder
+{
+    /**
+     * @var Template
+     */
+    protected $templateContainer;
+
+    /**
+     * @var IdentityInterface
+     */
+    protected $identityContainer;
+
+    /**
+     * @var TransportBuilder
+     */
+    protected $transportBuilder;
+
+    /**
+     * @var TransportBuilderByStore
+     */
+    private $transportBuilderByStore;
+
+    /**
+     * SenderBuilder constructor.
+     * @param Template $templateContainer
+     * @param IdentityInterface $identityContainer
+     * @param TransportBuilder $transportBuilder
+     * @param TransportBuilderByStore|null $transportBuilderByStore
+     */
+    public function __construct(
+        Template $templateContainer,
+        IdentityInterface $identityContainer,
+        TransportBuilder $transportBuilder,
+        TransportBuilderByStore $transportBuilderByStore = null
+    ) {
+        $this->templateContainer = $templateContainer;
+        $this->identityContainer = $identityContainer;
+        $this->transportBuilder = $transportBuilder;
+        $this->transportBuilderByStore = $transportBuilderByStore ?: ObjectManager::getInstance()->get(
+            TransportBuilderByStore::class
+        );
+    }
+
+    /**
+     * Prepare and send email message
+     *
+     * @return void
+     */
+    public function send()
+    {
+        $this->configureEmailTemplate();
+
+        $this->transportBuilder->addTo(
+            $this->identityContainer->getCustomerEmail(),
+            $this->identityContainer->getCustomerName()
+        );
+
+        $copyTo = $this->identityContainer->getEmailCopyTo();
+
+        if (!empty($copyTo) && $this->identityContainer->getCopyMethod() == 'bcc') {
+            foreach ($copyTo as $email) {
+                $this->transportBuilder->addBcc($email);
+            }
+        }
+
+        $transport = $this->transportBuilder->getTransport();
+        $transport->sendMessage();
+    }
+
+    /**
+     * Prepare and send copy email message
+     *
+     * @return void
+     */
+    public function sendCopyTo()
+    {
+        $copyTo = $this->identityContainer->getEmailCopyTo();
+
+        if (!empty($copyTo) && $this->identityContainer->getCopyMethod() == 'copy') {
+            foreach ($copyTo as $email) {
+                $this->configureEmailTemplate();
+
+                $this->transportBuilder->addTo($email);
+
+                $transport = $this->transportBuilder->getTransport();
+                $transport->sendMessage();
+            }
+        }
+    }
+
+    /**
+     * Configure email template
+     *
+     * @return void
+     */
+    protected function configureEmailTemplate()
+    {
+        $this->transportBuilder->setTemplateIdentifier($this->templateContainer->getTemplateId());
+        $this->transportBuilder->setTemplateOptions($this->templateContainer->getTemplateOptions());
+        $this->transportBuilder->setTemplateVars($this->templateContainer->getTemplateVars());
+        $this->transportBuilderByStore->setFromByStore(
+            $this->identityContainer->getEmailIdentity(),
+            $this->identityContainer->getStore()->getId()
+        );
+    }
+}
