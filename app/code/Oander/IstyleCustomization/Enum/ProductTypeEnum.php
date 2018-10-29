45c967d2425f642969d1637ab739eb26b3fdfadb
lajos <lajos.birnstein@oander.hu>
2018-10-29 12:57:22 +0100

    Missing files

---
+++ app/code/Oander/IstyleCustomization/Enum/ProductTypeEnum.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *   /$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$
+ *  /$$__  $$ /$$__  $$| $$$ | $$| $$__  $$| $$_____/| $$__  $$
+ * | $$  \ $$| $$  \ $$| $$$$| $$| $$  \ $$| $$      | $$  \ $$
+ * | $$  | $$| $$$$$$$$| $$ $$ $$| $$  | $$| $$$$$   | $$$$$$$/
+ * | $$  | $$| $$__  $$| $$  $$$$| $$  | $$| $$__/   | $$__  $$
+ * | $$  | $$| $$  | $$| $$\  $$$| $$  | $$| $$      | $$  \ $$
+ * |  $$$$$$/| $$  | $$| $$ \  $$| $$$$$$$/| $$$$$$$$| $$  | $$
+ *  \______/ |__/  |__/|__/  \__/|_______/ |________/|__/  |__/
+ *
+ * @license Oander Media Kft. (http://www.oander.hu)
+ */
+declare(strict_types=1);
+
+namespace Oander\IstyleCustomization\Enum;
+/**
+ * Class ProductTypeEnum
+ *
+ * @package Oander\IstyleCustomization\Enum
+ */
+final class ProductTypeEnum
+{
+    const PRODUCT_TYPE_ID_INSURANCE = 'insurance';
+}
