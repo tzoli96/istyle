f4c7442ef8cb7abefab5b42e8379f0d92d2ec420
vegvari42 <tamas@vegvari.net>
2019-02-04 15:14:50 +0100

    Oander\ApplePay\Controller\Ajax\PaymentRequest

--- app/code/Oander/ApplePay/Block/Main.php
+++ app/code/Oander/ApplePay/Block/Main.php
@@ -39,7 +39,7 @@ class Main extends Template
 
     public function getQuoteURL()
     {
-        return $this->getUrl(\Oander\ApplePay\Controller\Ajax\PaymentRequest::ROUTE);
+        return ''; //return $this->getUrl(\Oander\ApplePay\Controller\Ajax\PaymentRequest::ROUTE);
     }
 
     public function getMerchantId()
@@ -50,7 +50,7 @@ class Main extends Template
     public function getJsonConfig()
     {
         $config = $this->paymentConfig->getPaymentAllConfig();
-        $config['quoteDetailsURL'] = $this->getUrl(\Oander\ApplePay\Controller\Ajax\PaymentRequest::ROUTE);
+        $config['quoteDetailsURL'] = ''; // $config['quoteDetailsURL'] = $this->getUrl(\Oander\ApplePay\Controller\Ajax\PaymentRequest::ROUTE);
         $config['version'] = 4;
         $config['countryCode'] = $this->paymentConfig->getCountryCode();
         $config['languageCode'] = $this->paymentConfig->getLanguageCode();
