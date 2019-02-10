a4aab92c53f226890382d9430846c22d75560f91
pjohny <pjohny@freemail.hu>
2019-02-10 22:11:27 +0100

    tryinbank disable

--- app/etc/config.php
+++ app/etc/config.php
@@ -102,7 +102,7 @@ return array (
     'Innobyte_HomeCredit' => 1,
     'Magento_SalesInventory' => 1,
     'Amasty_Shiprestriction' => 1,
-    'Innobyte_InBank' => 1,
+    'Innobyte_InBank' => 0,
     'Magento_SampleData' => 0,
     'Magento_CatalogSearch' => 1,
     'Magento_Integration' => 1,
