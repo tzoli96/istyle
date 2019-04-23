696bbac819f76715733fb8b79d4d34aad4bec2f4
vegvari42 <tamas@vegvari.net>
2019-04-23 08:07:07 +0200

    #34834 | bundle price switcher

--- app/code/Oander/IstyleBase/Pricing/FinalPrice.php
+++ app/code/Oander/IstyleBase/Pricing/FinalPrice.php
@@ -2,11 +2,41 @@
 
 namespace Oander\IstyleBase\Pricing;
 
+use Magento\Framework\Pricing\Adjustment\CalculatorInterface;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Pricing\SaleableInterface;
+use Oander\BundlePriceSwitcher\Enum\Link as LinkEnum;
+use Oander\BundlePriceSwitcher\Helper\Config;
+
 /**
  * Class FinalPrice
  */
 class FinalPrice extends \Magento\Catalog\Pricing\Price\FinalPrice
 {
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * FinalPrice constructor.
+     *
+     * @param SaleableInterface      $saleableItem
+     * @param                        $quantity
+     * @param CalculatorInterface    $calculator
+     * @param PriceCurrencyInterface $priceCurrency
+     * @param Config                 $config
+     */
+    public function __construct(
+        SaleableInterface $saleableItem,
+        $quantity,
+        CalculatorInterface $calculator,
+        PriceCurrencyInterface $priceCurrency,
+        Config $config
+    ) {
+        parent::__construct($saleableItem, $quantity, $calculator, $priceCurrency);
+        $this->config = $config;
+    }
 
     /**
      * Get Value
@@ -15,6 +45,10 @@ class FinalPrice extends \Magento\Catalog\Pricing\Price\FinalPrice
      */
     public function getValue()
     {
+        if ($this->getProduct()->getData(LinkEnum::USE_REGULAR_PRICE) == 1 && $this->config->isEnabled()) {
+            return parent::getValue();
+        }
+
         $finalPrice = max(0, $this->getBasePrice()->getValue());
 
         $price = $this->product->getPrice();
--- app/code/Oander/IstyleBase/Pricing/RegularPrice.php
+++ app/code/Oander/IstyleBase/Pricing/RegularPrice.php
@@ -2,11 +2,41 @@
 
 namespace Oander\IstyleBase\Pricing;
 
+use Magento\Framework\Pricing\Adjustment\CalculatorInterface;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Pricing\SaleableInterface;
+use Oander\BundlePriceSwitcher\Enum\Link as LinkEnum;
+use Oander\BundlePriceSwitcher\Helper\Config;
+
 /**
  * Class RegularPrice
  */
 class RegularPrice extends \Magento\Catalog\Pricing\Price\RegularPrice
 {
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * RegularPrice constructor.
+     *
+     * @param SaleableInterface      $saleableItem
+     * @param                        $quantity
+     * @param CalculatorInterface    $calculator
+     * @param PriceCurrencyInterface $priceCurrency
+     * @param Config                 $config
+     */
+    public function __construct(
+        SaleableInterface $saleableItem,
+        $quantity,
+        CalculatorInterface $calculator,
+        PriceCurrencyInterface $priceCurrency,
+        Config $config
+    ) {
+        parent::__construct($saleableItem, $quantity, $calculator, $priceCurrency);
+        $this->config = $config;
+    }
 
     /**
      * Get price value
@@ -16,6 +46,10 @@ class RegularPrice extends \Magento\Catalog\Pricing\Price\RegularPrice
     public function getValue()
     {
         if ($this->value === null) {
+            if ($this->getProduct()->getData(LinkEnum::USE_REGULAR_PRICE) == 1 && $this->config->isEnabled()) {
+                return parent::getValue();
+            }
+
             $price = $this->product->getPrice();
             $oldPrice = $this->product->getOldPrice();
             $finalPrice = $this->product->getFinalPrice();
