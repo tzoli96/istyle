5038aef126307904622ec449f634d43cdee7e22b
vegvari42 <tamas@vegvari.net>
2019-12-03 13:16:46 +0100

    40210 email sender scope fix

---
+++ app/code/Oander/IstyleBase/Magento/Framework/Mail/Template/TransportBuilder.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *   /$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$
+ *  /$$__  $$ /$$__  $$| $$$ | $$| $$__  $$| $$_____/| $$__  $$
+ * | $$  \ $$| $$  \ $$| $$$$| $$| $$  \ $$| $$      | $$  \ $$
+ * | $$  | $$| $$$$$$$$| $$ $$ $$| $$  | $$| $$$$$   | $$$$$$$/
+ * | $$  | $$| $$__  $$| $$  $$$$| $$  | $$| $$__/   | $$__  $$
+ * | $$  | $$| $$  | $$| $$\  $$$| $$  | $$| $$      | $$  \ $$
+ * |  $$$$$$/| $$  | $$| $$ \  $$| $$$$$$$/| $$$$$$$$| $$  | $$
+ *  \______/ |__/  |__/|__/  \__/|_______/ |________/|__/  |__/
+ *
+ * Oander_IstyleBase
+ *
+ * TransportBuilderByStore
+ *
+ * @author  Róbert Betlen <robert.betlen@oander.hu>
+ * @license Oander Media Kft. (http://www.oander.hu)
+ */
+namespace Oander\IstyleBase\Magento\Framework\Mail\Template;
+
+class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
+{
+    /**
+     * @param $from
+     * @param $store
+     * @return $this
+     * @throws \Magento\Framework\Exception\MailException
+     */
+    public function setFromByScope($from, $store)
+    {
+        $result = $this->_senderResolver->resolve($from, $store);
+        $this->message->setFrom($result['email'], $result['name']);
+        return $this;
+    }
+}
