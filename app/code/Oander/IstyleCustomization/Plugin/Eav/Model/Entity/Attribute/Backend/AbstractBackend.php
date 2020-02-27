369b49c01180a82a12cd2b758efb12eac85a954e
vegvari42 <tamas@vegvari.net>
2020-02-27 15:59:29 +0100

    dobfix

--- app/code/Oander/IstyleCustomization/Plugin/Eav/Model/Entity/Attribute/Backend/AbstractBackend.php
+++ app/code/Oander/IstyleCustomization/Plugin/Eav/Model/Entity/Attribute/Backend/AbstractBackend.php
@@ -1,10 +1,40 @@
 <?php
 
-
 namespace Oander\IstyleCustomization\Plugin\Eav\Model\Entity\Attribute\Backend;
 
+use Magento\Framework\Exception\LocalizedException;
 
+/**
+ * Class AbstractBackend
+ * @package Oander\IstyleCustomization\Plugin\Eav\Model\Entity\Attribute\Backend
+ */
 class AbstractBackend
 {
+    /**
+     * Validate object
+     *
+     * @param \Magento\Framework\DataObject $object
+     * @return bool
+     * @throws LocalizedException
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function aroundValidate(
+        \Magento\Eav\Model\Entity\Attribute\Backend\AbstractBackend $subject,
+        \Closure $proceed,
+        $object
+    ) {
+        try {
+            $result = $proceed($object);
+        } catch (LocalizedException $exception) {
+            if (isset($exception->getParameters()[0]) &&
+                ($exception->getParameters()[0] == 'show_address_dob'
+                    || $exception->getParameters()[0] == 'show_pfpj_reg_no'
+                    || $exception->getParameters()[0] == 'address_dob')
+            ) {
+                $result = true;
+            }
+        }
 
+        return $result;
+    }
 }
