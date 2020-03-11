f1091335debfe14196213744a5e8d1501912d8ae
vegvari42 <tamas@vegvari.net>
2020-03-11 16:36:25 +0100

    checkout fix

--- app/code/Oander/OtpCalculator/view/frontend/web/js/payment/bigfishpaymentgateway_pmgw-mixin.js
+++ app/code/Oander/OtpCalculator/view/frontend/web/js/payment/bigfishpaymentgateway_pmgw-mixin.js
@@ -1,28 +1,29 @@
 define([], function () {
   'use strict';
-
   return function (BigfishPaymentGateway) {
     return BigfishPaymentGateway.extend({
-
       isCalculatorEnabled: function () {
-        return this.getProviderObj().calculator_enabled;
+        var provider = this.getProviderObj();
+        if (provider != null && parseInt(provider.calculator_enabled) === 1) {
+          return 1;
+        }
+        return 0;
       },
-
       getIframeURL: function () {
         var url = 'https://aruhitel.otpbank.hu/webshop/webshop-calculator.html';
         var grandTotal = Math.round(window.checkoutConfig.quoteData.base_grand_total);
         var provider = this.getProviderObj();
         // "bigfishpaymentgateway_pmgw_otparuhitel"
-
         //https://aruhitel.otpbank.hu/webshop/webshop-calculator.html?purchasePrice=389900&constructionGroup=1000071&retailerId=1003692
-
+        if (provider == null) {
+          return null;
+        }
         return url += '' +
           '?purchasePrice=' + grandTotal + '' +
           '&constructionGroup=' + provider.construction_group + '' +
           '&retailerId=' + provider.retailer_id + '' +
           '&term=' + provider.term;
       },
-
       getProviderObj: function () {
         var providers = window.checkoutConfig.payment.bigfishpaymentgateway_pmgw.providers;
         for (var i = 0; i < providers.length; i++) {
@@ -30,6 +31,7 @@ define([], function () {
             return providers[i];
           }
         }
+        return null;
       }
     });
   }
