4c9f2e7fb217ad367930c540df2c4dddb537526c
pjohny <pjohny16@gmail.com>
2020-09-17 17:05:13 +0200

    Update beforeinstall.sh

--- bin/beforeinstall.sh
+++ bin/beforeinstall.sh
@@ -3,8 +3,22 @@
 /etc/init.d/php7.0-fpm stop
 /etc/init.d/nginx stop
 
+SLACK_WEBHOOK="https://hooks.slack.com/services/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"
 WEBROOT="/var/www/istyle.eu/"
 LOGDIR="/var/log/magento"
+
+send_to_slack() {
+  local MESSAGE="${1}"
+  echo "$MESSAGE"
+  PAYLOAD="payload={
+    \"channel\": \"#istyle-collab\",
+    \"username\": \"${INSTANCE_ID}\",
+    \"text\": \"$MESSAGE\"
+    }"
+  curl -X POST --data-urlencode "${PAYLOAD}" ${SLACK_WEBHOOK} &> /dev/null
+}
+
+send_to_slack "iStyle Deploy started on DEV"
 
 if [ -d ${WEBROOT} ]; then
     rm -rf ${WEBROOT}
