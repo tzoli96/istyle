67c65f7b6a64fe343ee272a89311ab52dfff059e
janos <janos.pinczes@oander.hu>
2020-10-12 16:24:36 +0200

    #46820

--- app/code/Oander/IstyleCustomization/view/frontend/web/js/action/header-account.js
+++ app/code/Oander/IstyleCustomization/view/frontend/web/js/action/header-account.js
@@ -1,31 +1,33 @@
 require([
-    'jquery',
-    'Magento_Customer/js/customer-data',
-    'domReady!'
-], function ($, customerData) {
-    'use strict';
-    const options = {
-        url: '/istylecustomization/customer/header',
-        headerId: '#header-account-toggle',
-        iconClass: ".icon-user",
-        monogramClass: ".monogram-user"
-    };
-    var customer = customerData.get('customer');
-    customer.subscribe(function (updatedCustomer)
-    {
-        if(updatedCustomer.monogram !== undefined) {
-            if (updatedCustomer && $(options.headerId + " " + options.iconClass).is(":visible")) {
-                $(options.headerId + " " + options.iconClass).hide();
-                if ($(options.headerId + " " + options.monogramClass).length) {
-                    $(options.headerId + " " + options.monogramClass).show();
-                } else {
-                    $(options.headerId).html("<span class='" + options.monogramClass + "'>" + updatedCustomer.monogram + "</span>");
-                }
+    'jquery'
+], function ($) {
+    $(window).on("load", function () {
+        require([
+            'Magento_Customer/js/customer-data',
+        ], function (customerData) {
+            const options = {
+                url: '/istylecustomization/customer/header',
+                headerId: '#header-account-toggle',
+                iconClass: ".icon-user",
+                monogramClass: ".monogram-user"
+            };
 
-            } else {
-                $(options.headerId + " " + options.monogramClass).hide();
-                $(options.headerId + " " + options.iconClass).show();
-            }
-        }
-    }, this);
+            var customer = customerData.get('customer');
+            customer.subscribe(function (updatedCustomer) {
+                if (updatedCustomer.monogram !== undefined) {
+                    if (updatedCustomer && $(options.headerId + " " + options.iconClass).is(":visible")) {
+                        $(options.headerId + " " + options.iconClass).hide();
+                        if ($(options.headerId + " " + options.monogramClass).length) {
+                            $(options.headerId + " " + options.monogramClass).show();
+                        } else {
+                            $(options.headerId).html("<span class='" + options.monogramClass + "'>" + updatedCustomer.monogram + "</span>");
+                        }
+                    } else {
+                        $(options.headerId + " " + options.monogramClass).hide();
+                        $(options.headerId + " " + options.iconClass).show();
+                    }
+                }
+            }, this);
+        });
+    });
 });
