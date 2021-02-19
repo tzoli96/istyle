4f03df0d870dbc8b47ad89f46b8c4541c1a4bcbc
tzoli <turi.zoltan96@gmail.com>
2021-02-19 10:03:58 +0100

    Revert "fix"
    This reverts commit fb43de45

--- app/code/Oander/HelloBankPayment/Model/Ui/ConfigProvider.php
+++ app/code/Oander/HelloBankPayment/Model/Ui/ConfigProvider.php
@@ -7,6 +7,7 @@ use Oander\HelloBankPayment\Gateway\Config\ConfigValueHandler;
 use Magento\Checkout\Model\Session;
 use Oander\HelloBankPayment\Enum\Attribute;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Oander\HelloBankPayment\Api\Data\BaremRepositoryInterface;
 use Oander\HelloBankPayment\Api\Data\BaremInterface;
 
 class ConfigProvider implements ConfigProviderInterface
@@ -24,6 +25,11 @@ class ConfigProvider implements ConfigProviderInterface
     private $helloBankPaymentConfig;
 
     /**
+     * @var BaremRepositoryInterface
+     */
+    private $baremRepository;
+
+    /**
      * @var Session
      */
     private $checkoutSession;
@@ -36,17 +42,20 @@ class ConfigProvider implements ConfigProviderInterface
     /**
      * ConfigProvider constructor.
      *
+     * @param BaremRepositoryInterface $baremRepository
      * @param ProductRepositoryInterface $productRepository
      * @param ConfigValueHandler $helloBankPaymentConfig
      * @param StoreInterface $store
      * @param Session $checkoutSession
      */
     public function __construct(
+        BaremRepositoryInterface $baremRepository,
         ProductRepositoryInterface $productRepository,
         ConfigValueHandler $helloBankPaymentConfig,
         StoreInterface $store,
         Session $checkoutSession
     ) {
+        $this->baremRepository = $baremRepository;
         $this->productRepository = $productRepository;
         $this->helloBankPaymentConfig = $helloBankPaymentConfig;
         $this->store = $store;
@@ -61,7 +70,8 @@ class ConfigProvider implements ConfigProviderInterface
         return [
             'payment' => [
                 self::CODE => [
-                    'logoSrc' => $this->getLogoSrc()
+                    'logoSrc' => $this->getLogoSrc(),
+                    'barems'  => $this->getBarems()
                 ],
             ]
         ];
@@ -73,6 +83,53 @@ class ConfigProvider implements ConfigProviderInterface
     private function getLogoSrc(): string
     {
         return $this->helloBankPaymentConfig->getLogoSrc();
+    }
+
+    /**
+     * todo: repository helyett quoteból lekérdezni
+     * @return array
+     */
+    private function getBarems()
+    {
+        $items = $this->checkoutSession->getQuote()->getAllVisibleItems();
+        $barems = [];
+        foreach($items as $item)
+        {
+            $baremAttribute = $this->productRepository
+                ->get($item->getSku())
+                ->getCustomAttribute(Attribute::PRODUCT_BAREM_CODE);
+
+            if (null !== $baremAttribute && !in_array($baremAttribute->getValue(),$barems))
+            {
+                $baremIds=$baremAttribute->getValue();
+                if (strpos($baremIds, ',') !== false)
+                {
+                    $baremIds = explode(',', $baremIds);
+                    foreach ($baremIds as $baremId)
+                    {
+                        $barem=$this->baremRepository->getById($baremId);
+                        $barems[] = [
+                            "id"        => $barem->getId(),
+                            "name"      => $barem->getName(),
+                            "min_price" => $barem->getMinPrice(),
+                            "max_price" => $barem->getMaxPrice(),
+                            "priority"  => $barem->getPriority(),
+                        ];
+                    }
+                } else {
+                    $barem = $this->baremRepository->getById($baremIds);
+                    $barems[] = [
+                        "id"        => $barem->getId(),
+                        "name"      => $barem->getName(),
+                        "min_price" => $barem->getMinPrice(),
+                        "max_price" => $barem->getMaxPrice(),
+                        "priority"  => $barem->getPriority(),
+                    ];
+                }
+            }
+
+        }
+        return $barems;
     }
 
 
