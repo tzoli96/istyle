01be085955072a262818e15884e62689c3272bd3
kokaipatrik <steveepatrik@gmail.com>
2021-03-18 15:19:39 +0100

    50176 hellobank fe

--- app/code/Oander/HelloBankPayment/Block/Barem/Display.php
+++ app/code/Oander/HelloBankPayment/Block/Barem/Display.php
@@ -4,8 +4,11 @@ namespace Oander\HelloBankPayment\Block\Barem;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Template;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Directory\Model\CurrencyFactory;
 use Oander\HelloBankPayment\Api\Data\BaremRepositoryInterface;
 use Oander\HelloBankPayment\Api\Data\BaremInterface;
+use Oander\HelloBankPayment\Gateway\Config\ConfigValueHandler;
 use Oander\HelloBankPayment\Model\ResourceModel\Barems\CollectionFactory;
 use Magento\Framework\Registry;
 use Magento\Framework\Json\Helper\Data as JsonHelper;
@@ -19,9 +22,24 @@ class Display extends Template
     private $jsonHelper;
 
     /**
+     * @var StoreManagerInterface
+     */
+    private $storeConfig;
+
+    /**
+     * @var CurrencyFactory
+     */
+    private $currencyCode;
+
+    /**
      * @var CollectionFactory
      */
     private $baremCollection;
+
+    /**
+     * @var ConfigValueHandler
+     */
+    private $helloBankPaymentConfig;
 
     /**
      * @var Registry
@@ -37,14 +55,20 @@ class Display extends Template
     public function __construct(
         JsonHelper $jsonHelper,
         ProductRepositoryInterface $productRepsitory,
+        StoreManagerInterface $storeConfig,
+        CurrencyFactory $currencyFactory,
         Registry $registry,
         CollectionFactory $baremCollection,
+        ConfigValueHandler $helloBankPaymentConfig,
         Template\Context $context,
         array $data = []
     ) {
         $this->jsonHelper = $jsonHelper;
         $this->productRepsitory = $productRepsitory;
+        $this->storeConfig = $storeConfig;
+        $this->currencyCode = $currencyFactory->create();
         $this->baremCollection = $baremCollection;
+        $this->helloBankPaymentConfig = $helloBankPaymentConfig;
         $this->registry = $registry;
         parent::__construct($context, $data);
     }
@@ -71,7 +95,7 @@ class Display extends Template
     /**
      * @return array
      */
-    private function getBaremsData()
+    public function getBaremsData()
     {
 
         if($this->product()->getTypeId() == "configurable")
@@ -134,4 +158,51 @@ class Display extends Template
             ->addFieldToFilter(BaremInterface::ID, ['nin' => $params]);
     }
 
+    /**
+     * @return string
+     */
+    public function getSellerId()
+    {
+        return $this->helloBankPaymentConfig->getSellerId();
+    }
+    
+    /**
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->helloBankPaymentConfig->getIsActive();
+    }
+
+    /**
+     * @return string
+     */
+    public function getProductPrice()
+    {
+        /** @var Product $product */
+        $product = $this->registry->registry('current_product');
+
+        return round($product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue());
+    }
+
+    /**
+     * @return string
+     */
+    public function getSymbol()
+    {
+        $currentCurrency = $this->storeConfig->getStore()->getCurrentCurrencyCode();
+        $currency = $this->currencyCode->load($currentCurrency);
+
+        return $currency->getCurrencySymbol();
+    }
+
+    /**
+     * @return string
+     */
+    public function getProductType() {
+        /** @var Product $product */
+        $product = $this->registry->registry('current_product');
+
+        return $product->getTypeId();
+    }
 }
