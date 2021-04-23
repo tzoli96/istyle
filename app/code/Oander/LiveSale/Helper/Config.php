61c4d98c46c58836b05ec147dc704df07e736e2f
Janos <pjohny16@gmail.com>
2021-04-23 10:40:08 +0200

    #51107 | LiveSale modul

---
+++ app/code/Oander/LiveSale/Helper/Config.php
@@ -0,0 +1,13 @@
+<?php
+namespace Oander\LiveSale\Helper;
+
+class Config extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    public function getConfig($config_path)
+    {
+        return $this->scopeConfig->getValue(
+            $config_path,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+}
