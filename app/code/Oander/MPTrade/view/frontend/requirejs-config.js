b859aa012775aa597a2ad222272ab3f60fa242f5
kokaipatrik <steveepatrik@gmail.com>
2021-07-14 14:00:25 +0200

    51121 mptrade fe

---
+++ app/code/Oander/MPTrade/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+  map: {
+    'paths': {
+      'vue': 'Oander_MPTrade/js/vue',
+    }
+  }
+};
---
+++ app/code/Oander/MPTrade/view/frontend/web/js/helpers.js
@@ -0,0 +1,51 @@
+define([
+  'mage/translate',
+  'Magento_Catalog/js/price-utils',
+  'jquery',
+  'mage/calendar'
+], function ($t, priceUtils, $) {
+  return {
+    getFormattedPrice(price) {
+      var priceFormat = {
+        decimalSymbol: '.',
+        groupLength: 3,
+        groupSymbol: ",",
+        integerRequired: false,
+        pattern: "$%s",
+        precision: 2,
+        requiredPrecision: 2
+      };
+
+      return priceUtils.formatPrice(price, priceFormat);
+    },
+
+    translateString(string) {
+      return $t(string);
+    },
+
+    scrollTo(block, topMinus) {
+      var watch = setInterval(function () {
+        if ($(block).length) {
+          clearInterval(watch);
+
+          $('html, body').animate({
+            scrollTop: $(block).offset().top - topMinus,
+          }, 500);
+        }
+      }, 500);
+    },
+
+    toggleInfo: function () {
+      $(document).on('click', '.form-info-icon', function (e) {
+        var info = $(this).closest('.form-label').find('.form-info-content:not(.is-active)');
+        $('.form-info-content.is-active').removeClass('is-active');
+        info.addClass('is-active');
+      });
+
+      $(document).mouseup(function (e) {
+        var info = $('.form-info-content');
+        if (!info.is(e.target) && info.has(e.target).length === 0) info.removeClass('is-active');
+      });
+    },
+  };
+});
