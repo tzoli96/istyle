57721d3b5367f9b62db5f7c685b8f46ccb4f2d72
Janos <pjohny16@gmail.com>
2021-12-13 15:00:34 +0100

    #55726 | Stripe Extend

--- app/code/Oander/StripeIntegrationPaymentsExtend/Plugin/StripeIntegration/Payments/Api/Service.php
+++ app/code/Oander/StripeIntegrationPaymentsExtend/Plugin/StripeIntegration/Payments/Api/Service.php
@@ -39,4 +39,35 @@ class Service
         $result["requestShipping"] = true;
         return $result;
     }
+
+    public function aroundEstimate_cart(
+        \StripeIntegration\Payments\Api\Service $subject,
+        \Closure $proceed,
+        $address
+    ) {
+        $result = $proceed();
+        if(isset($address["shippingMethod"]))
+        {
+            if(isset($address["shippingMethod"]["carrier_code"]) && isset($address["shippingMethod"]["method_code"]))
+            {
+                $firstMethodId = null;
+                $result = \Zend_Json::decode($result);
+                foreach ($result["results"] as $id => $method)
+                {
+                    if($method["id"] == $address["shippingMethod"]["carrier_code"] . "_" . isset($address["shippingMethod"]["method_code"]))
+                    {
+                        $firstMethodId = $id;
+                    }
+                }
+                if($firstMethodId)
+                {
+                    $firstMethod = $result[$firstMethodId];
+                    array_splice($result,$firstMethod,1);
+                    array_unshift($result, $firstMethod);
+                }
+                $result = \Zend_Json::encode($result);
+            }
+        }
+        return $result;
+    }
 }
