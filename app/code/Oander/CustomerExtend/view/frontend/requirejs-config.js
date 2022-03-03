0aa20cf990752cd3e3b52b86709d9fb083cc2efc
László Krammer <laszlo.krammer@oander.hu>
2022-03-03 16:49:17 +0100

    56784 romanian zipcode - handle warning message

--- app/code/Oander/CustomerExtend/view/frontend/requirejs-config.js
+++ app/code/Oander/CustomerExtend/view/frontend/requirejs-config.js
@@ -7,5 +7,12 @@ var config = {
       select2: 'Oander_CustomerExtend/js/select2.min',
       roRegionCity: 'Oander_CustomerExtend/js/ro-region-city'
     }
+  },
+  config: {
+    mixins: {
+      'Magento_Checkout/js/model/shipping-rates-validator': {
+        'Oander_CustomerExtend/js/model/shipping-rates-validator-mixin': true
+      }
+    }
   }
 };
