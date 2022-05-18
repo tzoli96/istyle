84e849ea8b485798a75e058dd00ca37845f07d3a
Zoltan TUri <zoltan.turi@oander.hu>
2022-05-18 17:54:09 +0200

    57967 - 1.1.0

---
+++ app/code/Oander/ExternalRoundingUnit/Enum/Attribute.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oander\ExternalRoundingUnit\Enum;
+
+final class Attribute
+{
+    const EXTERNAL_ROUNDING_UNITE_QUOTE_ATTRIBUTE = "rounding_unit";
+    const EXTERNAL_ROUNDING_UNITE_ORDER_ATTRIBUTE = "rounding_unit";
+
+}
