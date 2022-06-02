2792596ba0b54249ad6c1807a83c9c2745b30580
zsolthegedus-oander <zsolt.hegedus@oander.hu>
2022-06-02 09:18:30 +0200

    58156 - questions undefined check

--- app/code/Oander/RaiffeisenPayment/view/frontend/web/js/view/payment/method-renderer/raiffeisen-method.js
+++ app/code/Oander/RaiffeisenPayment/view/frontend/web/js/view/payment/method-renderer/raiffeisen-method.js
@@ -48,7 +48,7 @@ define(
             },
 
             getEligibilityQuestions: function () {
-                var questions = (window.checkoutConfig.payment.raiffeisen.eligibilityquestions) ? JSON.parse(window.checkoutConfig.payment.raiffeisen.eligibilityquestions) : "";
+                var questions = (window.checkoutConfig.payment.raiffeisen.eligibilityquestions !== 'undefined') ? JSON.parse(window.checkoutConfig.payment.raiffeisen.eligibilityquestions) : "";
                 var self = this;
 
                 self.data = ko.observableArray([
