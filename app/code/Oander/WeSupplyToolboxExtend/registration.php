27ae004319b82374ebaffc5b6c68f79d1262e05a
János Pinczés <janos.pinczes@oander.hu>
2022-06-06 22:57:26 +0200

    #58124 - WeSupply Toolbox Extend fix wrong before plugin return value

--- app/code/Oander/WeSupplyToolboxExtend/registration.php
+++ app/code/Oander/WeSupplyToolboxExtend/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Jajuma_WebpImages',
+    'Oander_WeSupplyToolboxExtend',
     __DIR__
 );
